b9313f35da53db5db26f700cf61b1eea4b364c14
Robin Krahl <robin.krahl@ireas.org>
2020-04-02 17:04:51 +0200

    Refactor LongOperationInProgressException
    This patch changes LongOperationInProgressException to derive from
    DeviceCommunicationException.  This makes it easier to use the C API as
    the exception now has a unique numerical identifier.
    
    Fixes #170.

--- libnitrokey/LongOperationInProgressException.h
+++ libnitrokey/LongOperationInProgressException.h
@@ -22,23 +22,22 @@
 #ifndef LIBNITROKEY_LONGOPERATIONINPROGRESSEXCEPTION_H
 #define LIBNITROKEY_LONGOPERATIONINPROGRESSEXCEPTION_H
 
-#include "CommandFailedException.h"
+#include "DeviceCommunicationExceptions.h"
 
-class LongOperationInProgressException : public CommandFailedException {
+class LongOperationInProgressException : public DeviceCommunicationException {
 
 public:
     unsigned char progress_bar_value;
 
-    LongOperationInProgressException(
-        unsigned char _command_id, uint8_t last_command_status, unsigned char _progress_bar_value)
-    : CommandFailedException(_command_id, last_command_status), progress_bar_value(_progress_bar_value){
+    LongOperationInProgressException(unsigned char _progress_bar_value)
+    : DeviceCommunicationException("Device returned busy status with long operation in progress"),
+      progress_bar_value(_progress_bar_value){
       LOG(
           std::string("LongOperationInProgressException, progress bar status: ")+
               std::to_string(progress_bar_value), nitrokey::log::Loglevel::DEBUG);
     }
-    virtual const char *what() const throw() {
-      return "Device returned busy status with long operation in progress";
-    }
+
+    uint8_t getType() const {return 6;};
 };
 
 

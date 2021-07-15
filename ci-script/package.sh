79520646971ef0fa2f72c751d80a79cc1b1efa36
lennard.boediger <lennard@nitrokey.com>
2021-07-15 16:15:43 +0200

    add ci

---
+++ ci-script/package.sh
@@ -0,0 +1,40 @@
+ set -xe
+
+              mkdir output
+              OUTDIR="$(realpath output)"
+
+              BASENAME="libnitrokey"
+
+              pushd libnitrokey
+              VERSION="$(git describe --abbrev=0 ${GO_REVISION_LIBNITROKEY})"
+              BUILD="${VERSION}.${GO_PIPELINE_COUNTER:-0}.${GO_STAGE_COUNTER:-0}"
+              DATE="$(date -Iseconds)"
+
+              BUILD_TYPE="continuous"
+              git describe --exact-match ${GO_REVISION_LIBNITROKEY} &>/dev/null && BUILD_TYPE="release"
+              [ "${GO_TRIGGER_USER}" == "timer" ] && BUILD_TYPE="nightly"
+              
+              case "${BUILD_TYPE}" in
+                continuous)
+                  OUTNAME="${BASENAME}-${BUILD}"
+                  ;;
+                nightly)
+                  OUTNAME="${BASENAME}-${DATE}"
+                  ;;
+                release)
+                  OUTNAME="${BASENAME}-${VERSION}"
+                  ;;
+              esac
+              
+              git archive --format tar --prefix ${OUTNAME}/ ${GO_REVISION_LIBNITROKEY} | gzip > ${OUTDIR}/${OUTNAME}.tar.gz
+              popd
+
+              echo "LIBNITROKEY_BUILD_VERSION=\"${VERSION}\"" >> ./metadata
+              echo "LIBNITROKEY_BUILD_ID=\"${BUILD}\"" >> ./metadata
+              echo "LIBNITROKEY_BUILD_DATE=\"${DATE}\"" >> ./metadata
+              echo "LIBNITROKEY_BUILD_TYPE=\"${BUILD_TYPE}\"" >> ./metadata
+              echo "LIBNITROKEY_BUILD_OUTNAME=\"${OUTNAME}\"" >> ./metadata
+              
+              pushd ${OUTDIR}
+              sha256sum *.tar.gz > SHA256SUMS
+              popd

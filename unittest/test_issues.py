97234a695705c96c629028abc657f7bbb2073523
Szczepan Zalega <szczepan@nitrokey.com>
2021-08-26 17:27:22 +0200

    Test bootloader password issue from v0.14-RC2

--- unittest/test_issues.py
+++ unittest/test_issues.py
@@ -1,3 +1,7 @@
+from enum import Enum
+
+import pytest
+
 from constants import DefaultPasswords, DeviceErrorCode
 from misc import gs
 from test_pro import check_HOTP_RFC_codes
@@ -44,3 +48,32 @@ def test_destroy_encrypted_data_leaves_OTP_intact(C):
     # confirm OTP
     assert C.NK_first_authenticate(DefaultPasswords.ADMIN, DefaultPasswords.ADMIN_TEMP) == DeviceErrorCode.STATUS_OK
     assert gs(C.NK_get_hotp_slot_name(1)) == b'python_test'
+
+
+class Modes(Enum):
+    EmptyBody = 0
+    FactoryResetWithAES = 1
+    FactoryReset = 2
+    AESGeneration = 3
+
+@pytest.mark.firmware
+@pytest.mark.factory_reset
+@pytest.mark.parametrize("mode", map(Modes, range(4)))
+def test_pro_factory_reset_breaks_update_password(C, mode: Modes):
+    from test_pro_bootloader import test_bootloader_password_change_pro, test_bootloader_password_change_pro_length
+    from test_pro import test_factory_reset
+
+    func = {
+        0: lambda: True,
+        1: lambda: test_factory_reset(C) or True,
+        2: lambda: C.NK_factory_reset(DefaultPasswords.ADMIN) == DeviceErrorCode.STATUS_OK,
+        3: lambda: C.NK_build_aes_key(DefaultPasswords.ADMIN) == DeviceErrorCode.STATUS_OK,
+    }
+
+    def boot_test(C):
+        test_bootloader_password_change_pro(C)
+        test_bootloader_password_change_pro_length(C)
+
+    boot_test(C)
+    func[mode.value]()
+    boot_test(C)

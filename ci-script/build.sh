d474ceb57af64dabda8255b5c16708a21ffe3c2e
lennard.boediger <lennard@nitrokey.com>
2021-08-30 15:45:17 +0200

    rm tar.gz extract

--- ci-script/build.sh
+++ ci-script/build.sh
@@ -2,19 +2,19 @@
 set -exuo pipefail
 export
 
-. ./libnitrokey-source-metadata/metadata
-tar xf artifacts/${LIBNITROKEY_BUILD_OUTNAME}.tar.gz
+#. ./libnitrokey-source-metadata/metadata
+#tar xf artifacts/${LIBNITROKEY_BUILD_OUTNAME}.tar.gz
 
 
-pushd ${LIBNITROKEY_BUILD_OUTNAME}
-pip3 install --user -r unittest/requirements.txt
+#pushd ${LIBNITROKEY_BUILD_OUTNAME}
+#pip3 install --user -r unittest/requirements.txt
 
 ## This is quite sketchy but will work for now - we're using a tarball prepared by git archive, so we can't pull submodules.
 ## Instead, we download the Catch2 header manually. The version is hardcoded, which is bad.
 ## TODO: figure out a better way to handle that
 ## One possibility is to install Catch2 system-wide on builder images.
-mkdir -p unittest/Catch/single_include/catch2
-curl -L -o unittest/Catch/single_include/catch2/catch.hpp https://github.com/catchorg/Catch2/releases/download/v2.3.0/catch.hpp
+#mkdir -p unittest/Catch/single_include/catch2
+#curl -L -o unittest/Catch/single_include/catch2/catch.hpp https://github.com/catchorg/Catch2/releases/download/v2.3.0/catch.hpp
 
 mkdir build
 mkdir install
